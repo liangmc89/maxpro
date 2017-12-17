d0ec96ffed185f004d488306cdfbe5a430cdd64d
lmc89 <273187189@qq.com>
2017-12-18 00:32:36 +0800

    d

--- src/js/API.js
+++ src/js/API.js
@@ -10,7 +10,14 @@ class API {
 		commissionList:'/openapi/deposit/commissionList',		//返佣列表
 		followList:'/openapi/follow/index',						//跟单记录
 		withdraw:'/openapi/deposit/withdraw',					//佣金提现
-		dowithdraw:'/openapi/deposit/dowithdraw'                //佣金提现申请
+		dowithdraw:'/openapi/deposit/dowithdraw',               //佣金提现申请
+		depositInList:'/openapi/deposit/addmoney',			    //入金列表
+        depositOutList:'/openapi/deposit/addmoney',				//出金列表
+        depositDoin:'/openapi/deposit/doin',					//入金申请
+        depositDoout:'/openapi/deposit/doout',					//出金申请
+        depositIn:'/openapi/deposit/in',						//入金界面
+        depositOut:'/openapi/deposit/out',						//出金界面
+        getinfee:'/openapi/deposit/getinfee'					//入金手续费
 		
 
 	}
